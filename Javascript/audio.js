6a19acf90b38d7d47236c04910aaabac78a8256f
iker cordero <corderoiker9@gmail.com>
2026-02-14 21:02:04 -0400

    last commit 1422026

--- Javascript/audio.js
+++ Javascript/audio.js
@@ -1,19 +1,31 @@
- const musica = document.getElementById('Music');
-  const boton = document.getElementById('Music_button');
-  
-  //ajuste de volumen
-  musica.volume = 0.2; 
+const music = document.getElementById('Music');
+const boton = document.getElementById('Music_button');
+const iconSong = document.getElementById('icon_song');
+const iconOff = document.getElementById('icon_off');
 
-  boton.addEventListener('click', () => {
-    if (musica.paused) {
-      musica.play();
-    
-       //cambiar musica
-      icon_song.classList.add('bg-orange-200', `rounded-full`);
-      icon_song.classList.remove(`bg-orange-200`, `rounded-full`);
-     
-    } else {
-      musica.pause();
+music.volume = 0.2;
+
+boton.addEventListener('click', () => {
+
+  if (music.paused) {
    
-    }
-  });
+    music.play();
+    
+  
+    iconSong.classList.remove('hidden');
+    
+  
+    iconOff.classList.add('hidden');
+
+  } else {
+
+    music.pause();
+
+
+    iconSong.classList.add('hidden');
+
+   
+    iconOff.classList.remove('hidden');
+  }
+
+});
